c14238a63b3105bc1fbd539d5164f53c954b1a82
ggodes09 <gracegodes@gmail.com>
2016-11-11 16:12:11 -0500

    Create Codechef.java

---
+++ Codechef.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String word="hello world";
+	boolean isGirl=true;
+	if(isGirl==false)
+		System.out.println(word);
+		for(int x=2; x<=5; x+=2)
+		System.out.println(word);
+	}
+}
